1dd08695c54433d19eebe1896d416c659c917bce
terra819 <mrmrsgilbert@gmail.com>
2022-03-05 15:41:04 -0500

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,175 @@
-import { Component } from '@angular/core';
-import { of, Observable } from 'rxjs';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { KonvaComponent } from 'ng2-konva';
+import { of, Observable, BehaviorSubject } from 'rxjs';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  styles: ['canvas { border-style: solid }']
 })
 export class AppComponent {
+  @ViewChild('layer') layer: KonvaComponent;
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;  
+  
+  private ctx: CanvasRenderingContext2D;
   public configStage: Observable<any> = of({
-    width: 200,
-    height: 200,
+    width: 828,
+    height: 797,
   });
-  public configCircle: Observable<any> = of({
-    x: 100,
-    y: 100,
-    radius: 70,
-    fill: 'red',
-    stroke: 'black',
-    strokeWidth: 4,
-  });
-
-  public handleClick(component) {
-    console.log('Hello Circle', component);
+  public list = [];
+  public strips = [];
+  public circles = [
+    {
+      positionX: 417,
+      positionY: 400,
+      radius: 40,
+      pixels: [
+        { strip: 'A', index: 0 },
+        { strip: 'A', index: 1 },
+        { strip: 'A', index: 2 },
+        { strip: 'A', index: 3 },
+        { strip: 'B', index: 0 },
+        { strip: 'B', index: 1 },
+        { strip: 'B', index: 2 },
+        { strip: 'B', index: 3 },
+        { strip: 'C', index: 0 },
+        { strip: 'C', index: 1 },
+      ],
+      rValue: 255,
+      gValue: 0,
+      bValue: 0,
+    },
+    {
+      positionX: 418,
+      positionY: 351,
+      radius: 7,
+      pixels: [{ strip: 'A', index: 4 }],
+      rValue: 255,
+      gValue: 255,
+      bValue: 0,
+    },
+    {
+      positionX: 435,
+      positionY: 353,
+      radius: 7,
+      pixels: [{ strip: 'A', index: 5 }],
+      rValue: 255,
+      gValue: 0,
+      bValue: 0,
+    },
+    {
+      positionX: 450,
+      positionY: 363,
+      radius: 7,
+      pixels: [{ strip: 'A', index: 6 }],
+      rValue: 255,
+      gValue: 0,
+      bValue: 0,
+    },
+    {
+      positionX: 460,
+      positionY: 376,
+      radius: 7,
+      pixels: [{ strip: 'A', index: 7 }],
+      rValue: 255,
+      gValue: 0,
+      bValue: 0,
+    },
+    {
+      positionX: 466,
+      positionY: 392,
+      radius: 7,
+      pixels: [{ strip: 'A', index: 8 }],
+      rValue: 255,
+      gValue: 0,
+      bValue: 0,
+    },
+    {
+      positionX: 467,
+      positionY: 409,
+      radius: 7,
+      pixels: [{ strip: 'A', index: 9 }],
+      rValue: 255,
+      gValue: 0,
+      bValue: 0,
+    },
+  ];
+  code = 'code';
+  ngOnInit() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.drawCircles();
   }
+  public drawCircles() {
+    this.list = [];
+    this.circles.forEach((circle) => {
+      this.drawCircle(circle);
+      this.drawCircleOnCanvas(circle);
+    });
+    this.generateCode();
+  }
+  protected drawCircleOnCanvas(circle) {
+    this.ctx.beginPath();
+    this.ctx.arc(circle.positionX, circle.positionY, circle.radius, 0, 2 * Math.PI);
+    this.ctx.fillStyle = 'red';
+    this.ctx.stroke();
+    this.ctx.fill();
+  }
+  protected drawCircle(circle) {
+    this.list.push(
+      new BehaviorSubject({
+        x: circle.positionX,
+        y: circle.positionY,
+        radius: circle.radius,
+        fill: this.rgbToHex(circle.rValue, circle.gValue, circle.bValue),
+        stroke: 'black',
+        strokeWidth: 2,
+      })
+    );
+  }
+  public generateCode() {
+    this.code = 'generated';
+    // get by strip
+    // then by color
+    // then pixel
+    var allPixels = [];
+    this.circles.forEach((circle) => {
+      circle.pixels.forEach((pixel) => {
+        allPixels.push({
+          strip: pixel.strip,
+          index: pixel.index,
+          rgb: [circle.rValue,circle.gValue,circle.bValue]
+        });
+      });
+    });
+    // var stripA = allPixels.filter(x => x.strip = "A");
+    // const products = [{"strip":"A","index":0,"rgb":[255,0,0]},{"strip":"A","index":1,"rgb":[255,0,0]},{"strip":"A","index":2,"rgb":[255,0,0]},{"strip":"A","index":3,"rgb":[255,0,0]},{"strip":"A","index":0,"rgb":[255,0,0]},{"strip":"A","index":1,"rgb":[255,0,0]},{"strip":"A","index":2,"rgb":[255,0,0]},{"strip":"A","index":3,"rgb":[255,0,0]},{"strip":"A","index":0,"rgb":[255,0,0]},{"strip":"A","index":1,"rgb":[255,0,0]},{"strip":"A","index":4,"rgb":[255,255,0]},{"strip":"A","index":5,"rgb":[255,0,0]},{"strip":"A","index":6,"rgb":[255,0,0]},{"strip":"A","index":7,"rgb":[255,0,0]},{"strip":"A","index":8,"rgb":[255,0,0]},{"strip":"A","index":9,"rgb":[255,0,0]}];
+    
+    var groupByStrip = allPixels.reduce((group, product) => {
+      const { strip } = product;
+      group[strip] = group[strip] ?? [];
+      group[strip].push({"index": product.index, "rgb": product.rgb});
+      return group;
+    }, {});
+//{"A":[{"index":0,"rgb":[255,0,0]},{"index":1,"rgb":[255,0,0]},{"index":2,"rgb":[255,0,0]},{"index":3,"rgb":[255,0,0]},{"index":0,"rgb":[255,0,0]},{"index":1,"rgb":[255,0,0]},{"index":2,"rgb":[255,0,0]},{"index":3,"rgb":[255,0,0]},{"index":0,"rgb":[255,0,0]},{"index":1,"rgb":[255,0,0]},{"index":4,"rgb":[255,255,0]},{"index":5,"rgb":[255,0,0]},{"index":6,"rgb":[255,0,0]},{"index":7,"rgb":[255,0,0]},{"index":8,"rgb":[255,0,0]},{"index":9,"rgb":[255,0,0]}]}
+
+    
+    this.code = JSON.stringify(groupByStrip);
+  //   int stripARed[2] = {0,3};
+  // setStripAColor(stripARed, 255,0,0);
+  }
+
+  
+
+
+  public rgbToHex = (r, g, b) =>
+    '#' +
+    [r, g, b]
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length === 1 ? '0' + hex : hex;
+      })
+      .join('');
 }
